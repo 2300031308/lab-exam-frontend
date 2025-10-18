0d13be0887f36cb6faaaab7746c2442dde058bb5
2300031308 <162705885+2300031308@users.noreply.github.com>
2025-10-18 10:27:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 9099,             // 👈 This makes frontend run on port 9099
+  },
 })
